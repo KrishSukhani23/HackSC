3cbe7d1d729a16a2f2c50eced6bb6442d9e2f15e
Akshay Padte <akshaypadte99@gmail.com>
2023-02-04 23:24:13 -0800

    feat: send message in chunks via urls

--- chrome-extension/static/chat_script.js
+++ chrome-extension/static/chat_script.js
@@ -1,9 +1,41 @@
 console.log(document.getElementById("personName"));
-document.getElementById("personName").innerHTML = "Sending to "+localStorage.getItem("chattingWith");
+const friendName = localStorage.getItem("chattingWith");
+document.getElementById("personName").innerHTML = "Sending to " + friendName;
 
-const submitBtn = document.getElementById("submitBtn");
+const sendBtn = document.getElementById("sendBtn");
 
-submitBtn.onclick = (e) => {
-    const message = document.getElementById("message").value;
-    
-}
+sendBtn.onclick = async (e) => {
+  const message = document.getElementById("message").value;
+  const friends = localStorage.getItem("friends");
+  const serverUrl = document.getElementById("serverUrl");
+  const sitemap = (await (await fetch(serverUrl + '/sitemap.txt').res()));
+  const lines = sitemap.split('\n');
+
+  const indexMap = {};
+  for(const i = 0; i < lines.length; i++) {
+    indexMap[i] = lines[i];
+  }
+  if (friends) {
+    try {
+      const friendsObj = JSON.parse(friends);
+      const key = friendsObj[friendName];
+      const [enc_key, iv, start, end] = key.split(";");
+      if (!enc_key || !iv || !start || !end) {
+        console.log("invalid key!");
+      } else {
+        const cryptoKey = CryptoJS.enc.Hex.parse(enc_key);
+        const cryptoIV = CryptoJS.enc.Hex.parse(iv);
+        const encrypted = CryptoJS.AES.encrypt(message, cryptoKey, { iv: cryptoIV });
+
+        console.log(start + encrypted.ciphertext + end);
+        const toSend = start + encrypted.ciphertext + end;
+        for(const i = 0; i < toSend.length; i++) {
+            const reply = (await (await fetch(indexMap[parseInt(toSend[i], 16)])).res());
+            console.log(reply);
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
